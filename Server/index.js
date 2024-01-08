b6d59602b64d9d10f14d3b1b285a0255a2425024
venkateswarluSunkea <sunke@referral-loop.io>
2024-01-08 15:29:18 +0530

    changes

---
+++ Server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const rooms=[]
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data.room);
+
+    if(!rooms[data.room]){
+        rooms[data.room]=[]
+    };
+    rooms[data.room].push(data.username)
+
+    socket.to(data.room).emit("userslist",{users:rooms[data.room]});
+
+    console.log(rooms);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
